124765c7d1739fc6b7f9e1e492033c712c09502f
regiamariana <reginhamari03@gmail.com>
2021-02-10 00:48:46 -0300

    up

--- web-app/src/pages/home.js
+++ web-app/src/pages/home.js
@@ -3,13 +3,15 @@ import './home.css'
 import {Link} from "react-router-dom";
 import menu from '../assets/menu.png'
 import fundo from '../assets/bannerSite.jpg'
-
+import titulo from '../assets/titulo.png'
+import camera from '../assets/camerinha.png'
+import flecha from '../assets/flechasBaixo.png'
 function Home(){
     return(
         <div >
            <body>
                <header>
-                    <input type="checkbox" id="check" />
+                    {/* <input type="checkbox" id="check" />
                         <label id="icone" for="check"><img src={menu} /></label>
                         <div className="barra">
                             <nav>
@@ -18,12 +20,30 @@ function Home(){
                                 <Link><div className="link">Sobre</div></Link>
                                 
                             </nav>
-                        </div>
+                        </div> */}
                </header>
 
                <main>
                    <div className="banner">
+                        <div className="meninasConteudo">
+                        <img src={titulo}/>
+                        
+                        </div>
 
+                        <div className="textoBannerAnterior">
+                        <div className="textoBanner">
+                            <h2>projeto de fotografia em comemoração aos 18 anos do SENAI info.</h2>
+
+                            <Link to="/galeria">
+                            <h3>ver galeria</h3>
+                            <img src={camera} />
+                            </Link>
+                        </div>
+                        </div>
+                        <div className="flechinhas">
+                            <img src={flecha} />
+                        </div>
+                        
                    </div>
                </main>
            </body>
